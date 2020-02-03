4ebaf7dbd72b28b6cf5218a595f919569a910b99
viordash <viordash@mail.ru>
2020-02-03 23:20:08 +0300

    fix logging in RichTextBoxTarget

--- Project/UsAcRe/UsAcRe/MainForm.cs
+++ Project/UsAcRe/UsAcRe/MainForm.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Windows.Forms;
 using NLog;
+using NLog.Windows.Forms;
 
 namespace UsAcRe {
 	public partial class MainForm : Form {
 		static readonly Logger logger = LogManager.GetCurrentClassLogger();
 		public MainForm() {
 			InitializeComponent();
+			RichTextBoxTarget.ReInitializeAllTextboxes(this);
 		}
 
 		private void btnStart_Click(object sender, EventArgs e) {
