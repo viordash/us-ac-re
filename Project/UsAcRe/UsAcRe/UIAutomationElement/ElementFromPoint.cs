82d284406fd6a0f01d4e3ca7657a14df27d1024d
viordash <viordash@mail.ru>
2020-02-07 00:22:47 +0300

    review ElementFromPoint

--- Project/UsAcRe/UsAcRe/UIAutomationElement/ElementFromPoint.cs
+++ Project/UsAcRe/UsAcRe/UIAutomationElement/ElementFromPoint.cs
@@ -1,48 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Automation;
 using UsAcRe.Helpers;
 using UsAcRe.WindowsSystem;
 
 namespace UsAcRe.UIAutomationElement {
-	[Serializable]
+
 	public class ElementFromPoint {
 		NLog.Logger logger = NLog.LogManager.GetLogger("UsAcRe.Trace");
-		List<UIElementPathItem> PathItems = new List<UIElementPathItem>();
 
+		AutomationElement Element;
+		public Point ElementCoord { get; set; }
 
-		public ElementFromPoint(Point clickedPoint) {
-
-			var el = RetrieveAutomationElementFromPoint(clickedPoint);
-
+		public ElementFromPoint(Point elementCoord) {
+			ElementCoord = elementCoord;
+			DetermineElementUnderPoint();
 		}
 
-		AutomationElement RetrieveAutomationElementFromPoint(Point clickedPoint) {
+		public override string ToString() {
+			if(Element != null) {
+				return string.Format($"{nameof(ElementFromPoint)} ({ElementCoord.X}, {ElementCoord.Y}). Name: {NamingHelpers.Escape(Element.Current.Name, 50)}, " +
+					$"{Element.Current.ControlType.ProgrammaticName}, {Element.Current.BoundingRectangle}");
+			} else {
+				return string.Format($"{nameof(ElementFromPoint)} ({ElementCoord.X}, {ElementCoord.Y}). No element");
+			}
+		}
+
+		void DetermineElementUnderPoint() {
 			try {
-				return AutomationElement.FromPoint(new System.Windows.Point(clickedPoint.X, clickedPoint.Y));
-			} catch { }
-			var hwnd = WinAPI.WindowFromPoint(new WinAPI.POINT(clickedPoint.X, clickedPoint.Y));
-			var elementByHandle = AutomationElement.FromHandle(hwnd);
-			return GetElementThatBoundingRectangleContainsPoint(elementByHandle, clickedPoint, 0);
+				logger.Info("DetermineElementUnderPoint: {0}", ElementCoord);
+				Element = AutomationElement.FromPoint(new System.Windows.Point(ElementCoord.X, ElementCoord.Y));
+			} catch {
+				var hwnd = WinAPI.WindowFromPoint(new WinAPI.POINT(ElementCoord.X, ElementCoord.Y));
+				var elementByHandle = AutomationElement.FromHandle(hwnd);
+				Element = GetElementThatBoundingRectangleContainsPoint(elementByHandle, 0);
+			}
 		}
 
-		AutomationElement GetElementThatBoundingRectangleContainsPoint(AutomationElement root, Point clickedPoint, int nestedLevel) {
+		AutomationElement GetElementThatBoundingRectangleContainsPoint(AutomationElement root, int nestedLevel) {
 			if(nestedLevel++ > 10) {
 				return null;
 			}
 			foreach(var el in EnumerateChildElementsBack(root)) {
 				logger.Trace("             GetElementThatBoundingRectangleContainsPoint {3}: {0}; {1}; {2}", NamingHelpers.Escape(el.Current.Name, 300), el.Current.ControlType.ProgrammaticName,
 						el.Current.BoundingRectangle, nestedLevel);
-				if(el.Current.BoundingRectangle.Contains(clickedPoint.X, clickedPoint.Y)) {
-					return GetElementThatBoundingRectangleContainsPoint(el, clickedPoint, nestedLevel);
+				if(el.Current.BoundingRectangle.Contains(ElementCoord.X, ElementCoord.Y)) {
+					return GetElementThatBoundingRectangleContainsPoint(el, nestedLevel);
 				}
 			}
 			foreach(var el in EnumerateChildElements(root)) {
 				logger.Trace("             GetElementThatBoundingRectangleContainsPoint FORWARD {3}: {0}; {1}; {2}", NamingHelpers.Escape(el.Current.Name, 300), el.Current.ControlType.ProgrammaticName,
 						el.Current.BoundingRectangle, nestedLevel);
-				if(el.Current.BoundingRectangle.Contains(clickedPoint.X, clickedPoint.Y)) {
-					return GetElementThatBoundingRectangleContainsPoint(el, clickedPoint, nestedLevel);
+				if(el.Current.BoundingRectangle.Contains(ElementCoord.X, ElementCoord.Y)) {
+					return GetElementThatBoundingRectangleContainsPoint(el, nestedLevel);
 				}
 			}
 			return nestedLevel == 1 ? null : root;
