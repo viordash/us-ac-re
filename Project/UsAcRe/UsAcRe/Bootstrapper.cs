1050ff55a6c5d33bfd0419e6b0265c7ad777883a
viordash <viordash@mail.ru>
2020-03-01 20:54:47 +0300

    add Dependecy injection

---
+++ Project/UsAcRe/UsAcRe/Bootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using CommonServiceLocator;
+using Unity;
+using Unity.ServiceLocation;
+
+namespace UsAcRe {
+	public class Bootstrapper {
+
+		public static UnityContainer Initialize() {
+			var container = new UnityContainer();
+
+			var serviceLocator = new UnityServiceLocator(container);
+			ServiceLocator.SetLocatorProvider(() => serviceLocator);    // Warning: do NOT remove serviceLocator local variable, do not inline "new UnityServiceLocator"!
+			return container;
+		}
+	}
+}
