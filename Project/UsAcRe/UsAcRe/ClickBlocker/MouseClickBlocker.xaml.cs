fe00ed3003798d321ea643138faa50ee9dee3b4c
viordash <viordash@mail.ru>
2020-04-22 22:02:56 +0300

    change animation behaviour

--- Project/UsAcRe/UsAcRe/ClickBlocker/MouseClickBlocker.xaml.cs
+++ Project/UsAcRe/UsAcRe/ClickBlocker/MouseClickBlocker.xaml.cs
@@ -1,28 +1,48 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
 
 namespace UsAcRe.ClickBlocker {
 	public partial class MouseClickBlocker : Window {
+
 		public MouseClickBlocker(double Size) {
 			InitializeComponent();
-			this.Width = this.Height = MainViewbox.Width = MainViewbox.Height = Size;
+			Width = Height = MainViewbox.Width = MainViewbox.Height = Size;
 			Rotation.CenterX = Rotation.CenterY = Size / 2.0;
+			Animate();
 		}
 
 		public double Size {
-			get { return this.Width; }
+			get { return Width; }
 			set {
-				this.Width = this.Height = MainViewbox.Width = MainViewbox.Height = value;
+				Width = Height = MainViewbox.Width = MainViewbox.Height = value;
 				Rotation.CenterX = Rotation.CenterY = value / 2.0;
 			}
 		}
 
-		public void Rotate(double angle) {
-			Rotation.Angle += angle;
+		public void Animate() {
+			var doubleAnimation = new DoubleAnimation() {
+				From = 0.15,
+				To = 0.35,
+				AutoReverse = true,
+				Duration = new Duration(TimeSpan.FromSeconds(0.3)),
+				RepeatBehavior = new RepeatBehavior(1),
+				DecelerationRatio = 0.3
+			};
+
+			var storyboard = new Storyboard();
+			storyboard.Children.Add(doubleAnimation);
+			Storyboard.SetTargetName(doubleAnimation, ClickBlockerWindow.Name);
+			Storyboard.SetTargetProperty(doubleAnimation, new PropertyPath(OpacityProperty));
+			Storyboard.SetDesiredFrameRate(doubleAnimation, 60);
+			storyboard.Begin(this, true);
 		}
+
+
 
 		//FOR TESTS--------------------------------------------------------------------------------
 		private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e) {
-			Rotate(10);
+			Animate();
 		}
 		//-----------------------------------------------------------------------------------------
 	}
