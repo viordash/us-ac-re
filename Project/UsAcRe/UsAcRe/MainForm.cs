7f9cc40eb8e826cc32500ed1e3c4ec43bc0869ef
viordash <viordash@mail.ru>
2020-04-25 18:36:35 +0300

    MainForm. add method for MoveOutFromPoint when execute mouse action

--- Project/UsAcRe/UsAcRe/MainForm.cs
+++ Project/UsAcRe/UsAcRe/MainForm.cs
@@ -5,6 +5,7 @@ using NLog.Windows.Forms;
 using UsAcRe.Highlighter;
 using UsAcRe.Services;
 using UsAcRe.UIAutomationElement;
+using UsAcRe.WindowsSystem;
 
 namespace UsAcRe {
 	public partial class MainForm : Form {
@@ -44,5 +45,33 @@ namespace UsAcRe {
 			txtLog.Clear();
 		}
 
+		public static void MoveOutFromPoint(int X, int Y) {
+			if(Program.MainFormHandle == IntPtr.Zero)
+				return;
+			WinAPI.RECT rect = new WinAPI.RECT();
+			if(!WinAPI.GetWindowRect(Program.MainFormHandle, ref rect))
+				return;
+			bool mouseClickInMainForm = rect.top <= Y && rect.bottom >= Y && rect.left <= X && rect.right >= X;
+			if(!mouseClickInMainForm) {
+				return;
+			}
+			var workScreen = Screen.FromPoint(new System.Drawing.Point(X, Y));
+			int newX = X;
+			int newY = Y;
+			int formWidth = (int)(rect.right - rect.left);
+			int formHeight = (int)(rect.bottom - rect.top);
+			if((newX + formWidth) > workScreen.Bounds.Right) {
+				newX = newX - formWidth - 20;
+			} else {
+				newX += 20;
+			}
+			if((newY + formHeight) > workScreen.Bounds.Bottom) {
+				newY = newY - formHeight - 20;
+			} else {
+				newY += 20;
+			}
+
+			WinAPI.SetWindowPos(Program.MainFormHandle, WinAPI.HWND_TOPMOST, newX, newY, formWidth, formHeight, 0x10);
+		}
 	}
 }
