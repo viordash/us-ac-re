34ca41b10ac3340d4fba98d41a96d17db17ab7bd
viordash <viordash@mail.ru>
2020-08-30 14:57:44 +0300

    improve text escaping

---
+++ Project/UsAcRe/UsAcRe.Tests/HelpersTests/NamingHelpersTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NUnit.Framework;
+using UsAcRe.Helpers;
+using UsAcRe.MouseProcess;
+using UsAcRe.WindowsSystem;
+
+namespace UsAcRe.Tests.MouseProcessTests {
+	[TestFixture]
+	public class NamingHelpersTests {
+
+		[Test]
+		public void Escape_Test() {
+			var escaped = NamingHelpers.Escape("\t\tHello world \r\n \"test\"", int.MaxValue);
+			Assert.That(escaped, Is.EqualTo("\\t\\tHello world \\r\\n \\\"test\\\""));
+		}
+	}
+}
--- Project/UsAcRe/UsAcRe/Helpers/NamingHelpers.cs
+++ Project/UsAcRe/UsAcRe/Helpers/NamingHelpers.cs
@@ -13,16 +13,20 @@ namespace UsAcRe.Helpers {
 				var i16 = Convert.ToUInt16(ch);
 				if(i16 == 0x0D) {
 					sb[i] = '\\';
-					sb.Insert(i + 1, "r");
+					sb.Insert(i + 1, 'r');
 				} else if(i16 == 0x0A) {
 					sb[i] = '\\';
-					sb.Insert(i + 1, "n");
+					sb.Insert(i + 1, 'n');
 				} else if(i16 == 0x09) {
 					sb[i] = '\\';
-					sb.Insert(i + 1, "t");
+					sb.Insert(i + 1, 't');
 				} else if(i16 < 0x20) {
 					sb[i] = '\\';
 					sb.Insert(i + 1, string.Format("0x{0:X2}", i16));
+				} else if(i16 == '\"') {
+					sb[i] = '\\';
+					sb.Insert(i + 1, '\"');
+					i++;
 				}
 			}
 			return sb.ToString();
