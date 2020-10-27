a0405c76665b36d2fdc934f85650c785f0fb02e2
Vitaliy Orazov <viordash@mail.ru>
2020-10-27 22:56:07 +0300

    replace "Player" project with a console project

---
+++ Project/UsAcRe/UsAcRe.Player/Options.cs
@@ -0,0 +1,9 @@
+﻿using CommandLine;
+
+namespace UsAcRe.Player {
+	public class Options {
+		[Value(0, Required = true, HelpText = "Input filename.")]
+		public string Filename { get; set; }
+	}
+
+}
---
+++ Project/UsAcRe/UsAcRe.Player/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CommandLine;
+using CommandLine.Text;
+
+namespace UsAcRe.Player {
+	class Program {
+		static void Main(string[] args) {
+
+
+			var parser = new Parser(with => with.HelpWriter = null);
+			var parserResult = parser.ParseArguments<Options>(args);
+			parserResult
+			  .WithParsed(opt => Run(opt))
+			  .WithNotParsed(x => {
+				  var helpText = HelpText.AutoBuild(parserResult, h => {
+					  h.AutoHelp = false;
+					  h.AutoVersion = true;
+					  return HelpText.DefaultParsingErrorsHandler(parserResult, h);
+				  }, e => e);
+				  Console.WriteLine(helpText);
+			  });
+
+		}
+
+		static void Run(Options options) {
+			Console.WriteLine(options.Filename);
+		}
+	}
+}
