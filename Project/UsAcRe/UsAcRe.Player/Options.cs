b40c814786e12ae185a4ad88a8b4ae9bbaa1dac1
Vitaliy Orazov <viordash@mail.ru>
2020-12-06 19:02:19 +0300

    Player. short name for cli options

--- Project/UsAcRe/UsAcRe.Player/Options.cs
+++ Project/UsAcRe/UsAcRe.Player/Options.cs
@@ -5,22 +5,22 @@ namespace UsAcRe.Player {
 		[Value(0, Required = true, HelpText = "Input filename.")]
 		public string Filename { get; set; }
 
-		[Option("results-path", Required = false, HelpText = "test results path", Default = ".\\Results")]
+		[Option('p', "results-path", Required = false, HelpText = "test results path", Default = ".\\Results")]
 		public string TestResultsPath { get; set; }
 
-		[Option("check-by-value", Required = false, HelpText = "check value on comparing", Default = true)]
+		[Option('v', "check-by-value", Required = false, HelpText = "check value when comparing", Default = true)]
 		public bool CheckByValue { get; set; }
 
-		[Option("click-tolerance", Required = false, HelpText = "click position tolerance, percent", Default = 50)]
+		[Option('c', "click-tolerance", Required = false, HelpText = "click position tolerance, percent", Default = 50)]
 		public int ClickPositionToleranceInPercent { get; set; }
 
-		[Option("location-tolerance", Required = false, HelpText = "location change tolerance, percent", Default = null)]
+		[Option('l', "location-tolerance", Required = false, HelpText = "location change tolerance, percent", Default = null)]
 		public int? LocationToleranceInPercent { get; set; }
 
-		[Option("search-nesting-level", Required = false, HelpText = "element search nesting level", Default = 5)]
+		[Option('n', "search-nesting-level", Required = false, HelpText = "element search nesting level", Default = 5)]
 		public int ElementSearchNestingLevel { get; set; }
 
-		[Option("screenshot", Required = false, HelpText = "take screenshot when test fails", Default = true)]
+		[Option('s', "screenshot", Required = false, HelpText = "take screenshot when test fails", Default = true)]
 		public bool Screenshot { get; set; }
 	}
 }
