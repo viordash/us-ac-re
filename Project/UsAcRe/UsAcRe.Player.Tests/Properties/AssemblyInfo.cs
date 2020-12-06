09019915b6a2dcbaa1c6a0c90257138800eb6448
Vitaliy Orazov <viordash@mail.ru>
2020-12-06 19:07:27 +0300

    Player. add tests projects

---
+++ Project/UsAcRe/UsAcRe.Player.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UsAcRe.Player.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UsAcRe.Player.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("84d604d1-f576-4cf1-adee-fa32b2c61e5f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Project/UsAcRe/UsAcRe.Player.Tests/ReportersTests/ReporterFactoryTests.cs
@@ -0,0 +1,42 @@
+﻿using Moq;
+using NUnit.Framework;
+using UsAcRe.Core.Exceptions;
+using UsAcRe.Player.Reporters;
+using UsAcRe.Player.Services;
+
+namespace UsAcRe.Player.Tests.ReportersTests {
+	[TestFixture]
+	public class ReporterFactoryTests {
+		Mock<IPlayerSettingsService> settingsServiceMock;
+
+		[SetUp]
+		public virtual void Setup() {
+			settingsServiceMock = new Mock<IPlayerSettingsService>();
+		}
+
+		[TearDown]
+		public virtual void TearDown() {
+		}
+
+		[Test]
+		public void Create_Wrong_ReportType_Throws_PlayerException_Test() {
+			settingsServiceMock
+				.SetupGet(x => x.Reporter)
+				.Returns(() => {
+					return (ReporterType)10000;
+				});
+			Assert.Throws<PlayerException>(() => ReporterFactory.Create(settingsServiceMock.Object));
+		}
+
+		[Test]
+		public void Create_XUnit_Test() {
+			settingsServiceMock
+				.SetupGet(x => x.Reporter)
+				.Returns(() => {
+					return ReporterType.xUnit;
+				});
+			var reporter = ReporterFactory.Create(settingsServiceMock.Object);
+			Assert.That(reporter, Is.InstanceOf<XUnitReporter>());
+		}
+	}
+}
