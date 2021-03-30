958cb9bd6f56142abe2fa95d79bf77e75741d8e1
Vitaliy Orazov <viordash@mail.ru>
2021-03-30 23:30:19 +0300

    add Blazor WebAssembly App project

---
+++ Project/UsAcRe/UsAcRe.Web/Server/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace UsAcRe.Web.Server.Models {
+	public class ApplicationUser : IdentityUser {
+	}
+}
