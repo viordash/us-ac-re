618483175df755fbcfec70682b42b1cc527e69f6
Vitaliy Orazov <viordash@mail.ru>
2021-03-31 22:40:05 +0300

    clean code

--- Project/UsAcRe/UsAcRe.Web/Server/Data/ApplicationDbContext.cs
+++ Project/UsAcRe/UsAcRe.Web/Server/Data/ApplicationDbContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using IdentityServer4.EntityFramework.Options;
+﻿using IdentityServer4.EntityFramework.Options;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
