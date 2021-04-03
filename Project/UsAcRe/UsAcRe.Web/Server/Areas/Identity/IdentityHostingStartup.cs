10effa13280971c142a804aec413077fa93e6c3a
Vitaliy Orazov <viordash@mail.ru>
2021-04-03 21:20:50 +0300

    add asp identity to project

---
+++ Project/UsAcRe/UsAcRe.Web/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UsAcRe.Web.Server.Data;
+using UsAcRe.Web.Server.Models;
+
+[assembly: HostingStartup(typeof(UsAcRe.Web.Server.Areas.Identity.IdentityHostingStartup))]
+namespace UsAcRe.Web.Server.Areas.Identity {
+	public class IdentityHostingStartup : IHostingStartup {
+		public void Configure(IWebHostBuilder builder) {
+			builder.ConfigureServices((context, services) => {
+			});
+		}
+	}
+}
---
+++ Project/UsAcRe/UsAcRe.Web/Server/Areas/Identity/Pages/Account/ForgotPasswordConfirmation.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UsAcRe.Web.Server.Areas.Identity.Pages.Account {
+	[AllowAnonymous]
+	public class ForgotPasswordConfirmation : PageModel {
+		public void OnGet() {
+		}
+	}
+}
