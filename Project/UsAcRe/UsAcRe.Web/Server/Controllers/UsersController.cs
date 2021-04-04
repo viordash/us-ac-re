583d6b9eed8d34ffc5de7629e508298254a7c8b7
Vitaliy Orazov <viordash@mail.ru>
2021-04-04 23:09:03 +0300

    WIP. fetch users list

---
+++ Project/UsAcRe/UsAcRe.Web/Server/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using UsAcRe.Web.Shared.Models;
+
+namespace UsAcRe.Web.Server.Controllers {
+	[Authorize]
+	[ApiController]
+	[Route("[controller]")]
+	public class UsersController : ControllerBase {
+		readonly ILogger<UsersController> logger;
+
+		public UsersController(ILogger<UsersController> logger) {
+			this.logger = logger;
+		}
+
+		[HttpGet]
+		public IEnumerable<UserAccountModel> Get() {
+			var rng = new Random();
+			return Enumerable.Range(1, 5).Select(index => new UserAccountModel {
+				UserName = DateTime.Now.AddDays(index).ToShortDateString(),
+				Email = rng.Next(-20, 55).ToString()
+			})
+			.ToArray();
+		}
+	}
+}
---
+++ Project/UsAcRe/UsAcRe.Web/Shared/Models/UserAccountModel.cs
@@ -0,0 +1,9 @@
+﻿namespace UsAcRe.Web.Shared.Models {
+	public class UserAccountModel {
+		public string UserName { get; set; }
+		public string Email { get; set; }
+		public bool EmailConfirmed { get; set; }
+		public int AccessFailedCount { get; set; }
+		public string Role { get; set; }
+	}
+}
