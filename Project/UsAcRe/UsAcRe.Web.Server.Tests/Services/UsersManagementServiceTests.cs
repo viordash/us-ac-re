a71b2d0514ceb7211f230be71e840de0390f06a1
Vitaliy Orazov <viordash@mail.ru>
2021-05-21 00:02:11 +0300

    WIP. add tests project for UsAcRe.Web.Server

---
+++ Project/UsAcRe/UsAcRe.Web.Server.Tests/Services/UsersManagementServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+
+namespace UsAcRe.Web.Server.Tests.ServicesTests {
+	[TestFixture]
+	public class UsersManagementServiceTests {
+		protected Mock<IServiceProvider> serviceProviderMock;
+
+		[SetUp]
+		public void Setup() {
+		}
+
+		[Test]
+		public void Get_User_Test() {
+
+			Assert.Pass();
+		}
+	}
+}
