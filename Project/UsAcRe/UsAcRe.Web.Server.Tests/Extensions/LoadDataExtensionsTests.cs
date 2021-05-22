6cc200f0b4a879905a8149740b123710ab23ea67
Vitaliy Orazov <viordash@mail.ru>
2021-05-23 00:01:29 +0300

    add/improve tests for LoadDataExtensions

--- Project/UsAcRe/UsAcRe.Web.Server.Tests/Extensions/LoadDataExtensionsTests.cs
+++ Project/UsAcRe/UsAcRe.Web.Server.Tests/Extensions/LoadDataExtensionsTests.cs
@@ -9,22 +9,20 @@ namespace UsAcRe.Web.Server.Tests.ServicesTests {
 	[TestFixture]
 	public class LoadDataExtensionsTests : DbContextFixture {
 
+		public override void SetUp() {
+			base.SetUp();
+			for(int i = 0; i < 10; i++) {
+				DbContext.Users.Add(new ApplicationUser() {
+					Id = $"{i}",
+					UserName = $"test{i}",
+					Email = $"email{i}",
+				});
+			}
+			DbContext.SaveChanges();
+		}
+
 		[Test]
 		public async ValueTask PerformLoadPagedData_Filter_Test() {
-			DbContext.Users.Add(new ApplicationUser() {
-				Id = "1",
-				UserName = "test1"
-			});
-			DbContext.Users.Add(new ApplicationUser() {
-				Id = "2",
-				UserName = "test2"
-			});
-			DbContext.Users.Add(new ApplicationUser() {
-				Id = "22",
-				UserName = "test22"
-			});
-			DbContext.SaveChanges();
-
 			var loadDataArgs = new LoadDataArgs() {
 				Filter = "Id=\"2\"",
 				Top = 10,
@@ -32,10 +30,55 @@ namespace UsAcRe.Web.Server.Tests.ServicesTests {
 			};
 
 			var users = await DbContext.Users.PerformLoadPagedData(loadDataArgs, "UserName");
-
 			Assert.That(users, Has.Count.EqualTo(1));
 			Assert.That(users[0].Id, Is.EqualTo("2"));
 			Assert.That(users[0].UserName, Is.EqualTo("test2"));
+
+			const string ignoreCaseFilter = "iD=\"4\"";
+			loadDataArgs.Filter = ignoreCaseFilter;
+			users = await DbContext.Users.PerformLoadPagedData(loadDataArgs, "UserName");
+			Assert.That(users, Has.Count.EqualTo(1));
+			Assert.That(users[0].Id, Is.EqualTo("4"));
+			Assert.That(users[0].UserName, Is.EqualTo("test4"));
+		}
+
+		[Test]
+		public async ValueTask PerformLoadPagedData_Offset_Test() {
+			var loadDataArgs = new LoadDataArgs() {
+				Top = 4,
+				Skip = 3
+			};
+
+			var users = await DbContext.Users.PerformLoadPagedData(loadDataArgs, "UserName");
+			Assert.That(users, Has.Count.EqualTo(4));
+			Assert.That(users[0].Id, Is.EqualTo("3"));
+			Assert.That(users[0].UserName, Is.EqualTo("test3"));
+		}
+
+		[Test]
+		public async ValueTask PerformLoadPagedData_OrderBy_Test() {
+			var loadDataArgs = new LoadDataArgs() {
+				Top = 10,
+				Skip = 0,
+				OrderBy = "email desc"
+			};
+
+			var users = await DbContext.Users.PerformLoadPagedData(loadDataArgs, "UserName");
+			Assert.That(users, Has.Count.EqualTo(10));
+			Assert.That(users[0].Id, Is.EqualTo("9"));
+			Assert.That(users[0].Email, Is.EqualTo("email9"));
+			Assert.That(users[9].Id, Is.EqualTo("0"));
+			Assert.That(users[9].Email, Is.EqualTo("email0"));
+
+			const string ignoreCaseOrderBy = "USERName asc";
+			loadDataArgs.OrderBy = ignoreCaseOrderBy;
+			users = await DbContext.Users.PerformLoadPagedData(loadDataArgs, "UserName");
+			Assert.That(users, Has.Count.EqualTo(10));
+			Assert.That(users[0].Id, Is.EqualTo("0"));
+			Assert.That(users[0].UserName, Is.EqualTo("test0"));
+			Assert.That(users[9].Id, Is.EqualTo("9"));
+			Assert.That(users[9].UserName, Is.EqualTo("test9"));
+
 		}
 	}
 }
