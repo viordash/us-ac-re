4ef4abcbc3412dd6a23d2cbf4f10d181b6daf0b7
Vitaliy Orazov <viordash@mail.ru>
2021-05-25 22:27:08 +0300

    merge DbContext descendant classes

--- Project/UsAcRe/UsAcRe.Web/Server/Data/ApplicationDbContext.cs
+++ Project/UsAcRe/UsAcRe.Web/Server/Data/ApplicationDbContext.cs
@@ -1,12 +1,32 @@
-﻿using IdentityServer4.EntityFramework.Options;
+﻿using System;
+using System.Threading.Tasks;
+using IdentityServer4.EntityFramework.Entities;
+using IdentityServer4.EntityFramework.Extensions;
+using IdentityServer4.EntityFramework.Interfaces;
+using IdentityServer4.EntityFramework.Options;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using UsAcRe.Web.Server.Identity;
 
 namespace UsAcRe.Web.Server.Data {
-	public class ApplicationDbContext : ApplicationAuthorizationDbContext {
-		public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
-			: base(options, operationalStoreOptions) {
+	public class ApplicationDbContext : ApplicationIdentityDbContext<ApplicationUser, ApplicationIdentityRole>, IPersistedGrantDbContext, IDisposable {
+		private readonly IOptions<OperationalStoreOptions> operationalStoreOptions;
+
+		public ApplicationDbContext(
+			DbContextOptions options,
+			IOptions<OperationalStoreOptions> operationalStoreOptions)
+			: base(options) {
+			this.operationalStoreOptions = operationalStoreOptions;
 		}
 
+		public DbSet<PersistedGrant> PersistedGrants { get; set; }
+		public DbSet<DeviceFlowCodes> DeviceFlowCodes { get; set; }
+
+		Task<int> IPersistedGrantDbContext.SaveChangesAsync() => base.SaveChangesAsync();
+
+		protected override void OnModelCreating(ModelBuilder builder) {
+			base.OnModelCreating(builder);
+			builder.ConfigurePersistedGrantContext(operationalStoreOptions.Value);
+		}
 	}
 }
