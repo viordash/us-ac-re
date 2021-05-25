59b7ddc3fedbe3a502dc9cdf0608181e39accd8a
Vitaliy Orazov <viordash@mail.ru>
2021-05-26 01:23:39 +0300

    add db migration for initial creation of default roles

---
+++ Project/UsAcRe/UsAcRe.Web/Server/Migrations/20210525220304_CreateDefaultRoles.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UsAcRe.Web.Server.Migrations {
+	public partial class CreateDefaultRoles : Migration {
+		protected override void Up(MigrationBuilder migrationBuilder) {
+			migrationBuilder.InsertData(
+				table: "AspNetRoles",
+				columns: new string[] { "Name", "NormalizedName", "ConcurrencyStamp" },
+				values: new object[] { "Administrator", "Administrator", System.Guid.NewGuid().ToString() }
+				);
+
+			migrationBuilder.InsertData(
+				table: "AspNetRoles",
+				columns: new string[] { "Name", "NormalizedName", "ConcurrencyStamp" },
+				values: new object[] { "Tenant", "Tenant", System.Guid.NewGuid().ToString() }
+				);
+
+			migrationBuilder.InsertData(
+				table: "AspNetRoles",
+				columns: new string[] { "Name", "NormalizedName", "ConcurrencyStamp" },
+				values: new object[] { "User", "User", System.Guid.NewGuid().ToString() }
+				);
+		}
+
+		protected override void Down(MigrationBuilder migrationBuilder) {
+			migrationBuilder.DeleteData(
+				table: "AspNetRoles",
+				keyColumn: "Name",
+				keyValue: "Administrator"
+				);
+
+			migrationBuilder.DeleteData(
+				table: "AspNetRoles",
+				keyColumn: "Name",
+				keyValue: "Tenant"
+				);
+
+			migrationBuilder.DeleteData(
+				table: "AspNetRoles",
+				keyColumn: "Name",
+				keyValue: "User"
+				);
+		}
+	}
+}
