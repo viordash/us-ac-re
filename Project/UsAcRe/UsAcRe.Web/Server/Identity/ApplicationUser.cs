15d4de586dd0d25dea37c8e227ca66b64d5080ba
Vitaliy Orazov <viordash@mail.ru>
2021-05-29 18:56:18 +0300

    remove excess field from ApplicationUser

--- Project/UsAcRe/UsAcRe.Web/Server/Identity/ApplicationUser.cs
+++ Project/UsAcRe/UsAcRe.Web/Server/Identity/ApplicationUser.cs
@@ -1,10 +1,6 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 
 namespace UsAcRe.Web.Server.Identity {
 	public class ApplicationUser : IdentityUser<System.Guid> {
-		[NotMapped]
-		public IEnumerable<string> RoleNames { get; set; }
 	}
 }
