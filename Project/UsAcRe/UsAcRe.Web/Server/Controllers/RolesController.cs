34cb0648fd77da40a45b7f96cbb9fdad9d5e6211
Vitaliy Orazov <viordash@mail.ru>
2021-06-08 23:40:07 +0300

    WIP. role controller

---
+++ Project/UsAcRe/UsAcRe.Web/Server/Controllers/RolesController.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using GuardNet;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UsAcRe.Web.Server.Identity;
+
+namespace UsAcRe.Web.Server.Controllers {
+	[Authorize]
+	[ApiController]
+	[Route("[controller]")]
+	public class RolesController : ControllerBase {
+		readonly RoleManager<ApplicationIdentityRole> roleManager;
+
+		public RolesController(
+			RoleManager<ApplicationIdentityRole> roleManager
+			) {
+			Guard.NotNull(roleManager, nameof(roleManager));
+			this.roleManager = roleManager;
+		}
+
+		[HttpGet]
+		public IActionResult Get() {
+			var roles = roleManager.Roles;
+			return new ObjectResult(roles);
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult Get(System.Guid id) {
+			var user = roleManager.Roles.FirstOrDefault(x => x.Id == id);
+			return new ObjectResult(user);
+		}
+	}
+}
