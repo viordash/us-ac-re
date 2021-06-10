df31d88c04ac85f95717ed8694fa1df5b9e2adef
Vitaliy Orazov <viordash@mail.ru>
2021-06-10 15:25:39 +0300

    add tests for ApplicationRoleStore

--- Project/UsAcRe/UsAcRe.Web.Server.Tests/Identity/ApplicationRoleStoreTests.cs
+++ Project/UsAcRe/UsAcRe.Web.Server.Tests/Identity/ApplicationRoleStoreTests.cs
@@ -1,23 +1,32 @@
 ﻿using System;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Identity;
-using Moq;
 using NUnit.Framework;
-using Radzen;
 using Tests.Common;
+using UsAcRe.Web.Server.Data;
 using UsAcRe.Web.Server.Identity;
-using UsAcRe.Web.Server.Services;
-using UsAcRe.Web.Shared.Exceptions;
 
 namespace UsAcRe.Web.Server.Tests.ServicesTests {
 	[TestFixture]
 	public class ApplicationRoleStoreTests : AuthorizationDbContextFixture {
-		ApplicationRoleStore testable;
+		#region inner classes
+		class PublicMorozov_ApplicationRoleStore : ApplicationRoleStore {
+			public PublicMorozov_ApplicationRoleStore(ApplicationDbContext context, ILookupNormalizer keyNormalizer)
+				: base(context, keyNormalizer) {
+			}
+
+			public ApplicationIdentityRoleClaim PublicCreateRoleClaim(ApplicationIdentityRole role, Claim claim) {
+				return CreateRoleClaim(role, claim);
+			}
+		}
+		#endregion
+
+		PublicMorozov_ApplicationRoleStore testable;
 
 		public override void SetUp() {
 			base.SetUp();
-			testable = new ApplicationRoleStore(DbContext, keyNormalizerMock.Object);
+			testable = new PublicMorozov_ApplicationRoleStore(DbContext, keyNormalizerMock.Object);
 		}
 
 		[Test]
@@ -30,6 +39,39 @@ namespace UsAcRe.Web.Server.Tests.ServicesTests {
 		[Test]
 		public void CreateAsync_Throws_NotSupportedException() {
 			Assert.ThrowsAsync<NotSupportedException>(async () => await testable.CreateAsync(new ApplicationIdentityRole(ApplicationRoleType.Administrator, "", "")));
+		}
+
+		[Test]
+		public void UpdateAsync_Throws_NotSupportedException() {
+			Assert.ThrowsAsync<NotSupportedException>(async () => await testable.UpdateAsync(new ApplicationIdentityRole(ApplicationRoleType.Administrator, "", "")));
+		}
+
+		[Test]
+		public void DeleteAsync_Throws_NotSupportedException() {
+			Assert.ThrowsAsync<NotSupportedException>(async () => await testable.DeleteAsync(new ApplicationIdentityRole(ApplicationRoleType.Administrator, "", "")));
+		}
+
+		[Test]
+		public void SetRoleNameAsync_Throws_NotSupportedException() {
+			Assert.ThrowsAsync<NotSupportedException>(async () => await testable.SetRoleNameAsync(new ApplicationIdentityRole(ApplicationRoleType.Administrator, "", ""), "role"));
+		}
+
+		[Test]
+		public void AddClaimAsync_Throws_NotSupportedException() {
+			Assert.ThrowsAsync<NotSupportedException>(async () => await testable.AddClaimAsync(new ApplicationIdentityRole(ApplicationRoleType.Administrator, "", ""),
+				new Claim("type", "value")));
+		}
+
+		[Test]
+		public void RemoveClaimAsync_Throws_NotSupportedException() {
+			Assert.ThrowsAsync<NotSupportedException>(async () => await testable.RemoveClaimAsync(new ApplicationIdentityRole(ApplicationRoleType.Administrator, "", ""),
+				new Claim("type", "value")));
+		}
+
+		[Test]
+		public void CreateRoleClaim_Throws_NotSupportedException() {
+			Assert.Throws<NotSupportedException>(() => testable.PublicCreateRoleClaim(new ApplicationIdentityRole(ApplicationRoleType.Administrator, "", ""),
+				new Claim("type", "value")));
 		}
 	}
 }
