457a35debcea355b17910cc0220a641386b02328
Vitaliy Orazov <viordash@mail.ru>
2021-07-03 01:40:06 +0300

    remove string field Filter

--- Project/UsAcRe/UsAcRe.Web/Shared/Models/DataPaging.cs
+++ Project/UsAcRe/UsAcRe.Web/Shared/Models/DataPaging.cs
@@ -4,7 +4,6 @@ namespace UsAcRe.Web.Shared.Models {
 	public class DataPaging {
 		public int? Skip { get; set; }
 		public int? Top { get; set; }
-		public string Filter { get; set; }
 		public IEnumerable<FilterDescriptor> Filters { get; set; }
 		public IEnumerable<SortDescriptor> Sorts { get; set; }
 	}
