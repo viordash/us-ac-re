9d83bd3c5fd4e812e9aeac6efe1af61a84dbceff
Vitaliy Orazov <viordash@mail.ru>
2021-08-18 02:00:58 +0300

    WIP. customize Radzen pager and grid

---
+++ Project/UsAcRe/UsAcRe.Web/Client/RadzenCustom/XRadzenComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace UsAcRe.Web.Client.RadzenCustom {
+	public class XRadzenComponent : RadzenComponent {
+
+		public new ElementReference Element { get; set; }
+
+	}
+
+}
---
+++ Project/UsAcRe/UsAcRe.Web/Client/RadzenCustom/XRadzenDataGrid.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Radzen;
+using Radzen.Blazor;
+
+namespace UsAcRe.Web.Client.RadzenCustom {
+	public class XRadzenDataGrid<TItem> : RadzenDataGrid<TItem> {
+
+		public new Task OnPageChanged(PagerEventArgs args) {
+			return base.OnPageChanged(args);
+		}
+
+		public new Task OnPageSizeChanged(int value) {
+			return base.OnPageSizeChanged(value);
+		}
+
+		int pageSize = 8;
+		protected int CurrentPageSize {
+			get {
+				return pageSize;
+			}
+		}
+
+		protected override void OnInitialized() {
+			if(PageSize == default) {
+				PageSize = pageSize;
+			}
+			pageSize = PageSize;
+		}
+	}
+}
